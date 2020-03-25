3679ddb59a1ceb6268fb2c3950459507bfe136a1
Rahul Chouhan <rchouhan791@gmail.com>
2020-03-25 12:39:11 +0530

    Added first html page

--- demoapp/views.py
+++ demoapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from .models import Demo
+
 # Create your views here.
+def demo_view(request, *args, **kwargs):
+	obj = Demo.objects.get(id=1)
+	return render(request, "demoapp/demo.html", {})
+
+
